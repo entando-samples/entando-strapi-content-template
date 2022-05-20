e6bc2e9ab1e21f37fd15e87c9146df8ac1a79d61
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-05-20 18:01:26 +0530

    added log.

--- ui/widgets/widgets-dir/et-strapi-template-widget/src/integration/StrapiAPI.js
+++ ui/widgets/widgets-dir/et-strapi-template-widget/src/integration/StrapiAPI.js
@@ -105,6 +105,7 @@ export const getContentTypes = async (conType) => {
             return el;
         }
     });
+    console.log("Only For Testing",filterListByConType)
     if (filterListByConType && filterListByConType.length && filterListByConType[0].schema) {
         return filterListByConType[0].schema.attributes;
     }
