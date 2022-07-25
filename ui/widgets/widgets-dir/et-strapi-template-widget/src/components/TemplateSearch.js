9df41afcacc5d17fbee7ac30dd6e4c54c0614ad9
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-07-25 15:46:01 +0530

    improved class name.

--- ui/widgets/widgets-dir/et-strapi-template-widget/src/components/TemplateSearch.js
+++ ui/widgets/widgets-dir/et-strapi-template-widget/src/components/TemplateSearch.js
@@ -33,28 +33,28 @@ export default class TemplateSearch extends Component {
 
     render() {
         return (
-            <div className="well tw-search-well-div">
-                <div className="tw-search-container-div">
+            <div className="well well-height">
+                <div className="well-container-position">
                     <div className="container-fluid">
                         <div className="show-grid row">
-                        <div className="col-lg-1 tw-search-label"><FormattedMessage id="app.search" /></div>
+                        <div className="col-lg-1 search-label-size"><FormattedMessage id="app.search" /></div>
                             <div className="col-lg-10"></div>
                         </div>
-                        <div className="show-grid row tw-search-show-grid-row-div">
+                        <div className="show-grid row tw-search-box-height">
                             <div className="col-lg-1"></div>
-                            <div className="col-lg-1 tw-type-label-div">
+                            <div className="col-lg-1 tw-type-label">
                                 <FormattedMessage id="app.type" />
                             </div>
-                            <select onChange={this.collectionTypeOnChange} className="col-lg-7 tw-search-select" name="cars" id="cars">
+                            <select onChange={this.collectionTypeOnChange} className="col-lg-7 tw-search">
                                 <FormattedMessage id='app.all' >
                                     {(message) => <option value='All'>{message}</option>}
                                 </FormattedMessage>
                                 {this.state.collectionType.map(el => <option key={el.displayName} value={el.displayName}>{el.displayName}</option>)}
                             </select>
                         </div>
-                        <div className="show-grid row tw-search-btn-mainDiv">
+                        <div className="show-grid row mt-1">
                             <div className="col-lg-7"></div>
-                            <div className="col-lg-4 tw-search-btn-col-lg-4-div">
+                            <div className="col-lg-4 ml-5">
                                 <button onClick={this.collectionTypeOnClick} className="btn btn-primary"><FormattedMessage id="app.search" /></button>
                             </div>
                         </div>
