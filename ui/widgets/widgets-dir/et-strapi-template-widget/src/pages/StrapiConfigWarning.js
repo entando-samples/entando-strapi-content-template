71516c921adebad1045e4b4706433e353c8c4322
Kamlesh Bobde <kamlesh.bobde@newvisionsoftware.in>
2022-08-15 19:46:50 +0530

    PBCS-142: Changes in .env and warning msg as per strapi config widget

--- ui/widgets/widgets-dir/et-strapi-template-widget/src/pages/StrapiConfigWarning.js
+++ ui/widgets/widgets-dir/et-strapi-template-widget/src/pages/StrapiConfigWarning.js
@@ -1,12 +1,15 @@
 import { Button } from 'patternfly-react';
 import React from 'react';
-import { BTN_RELOAD_PAGE, STRAPI_URL } from '../constant/constant';
+import { BTN_RELOAD_PAGE, STRAPI_CONFIG_WARNING_MSG } from '../constant/constant';
 export default function StrapiConfigWarning() {
     return (
         <div className="well">
             <div>
-                <h4><strong><span className="pficon pficon-warning-triangle-o"></span> {STRAPI_URL} can't be reached.
-                    Please click <a href={process.env.REACT_APP_STRAPI_CONFIG_FE_URL} target="_blank">here</a> to configure it.</strong>
+                <h4>
+                    <strong>
+                        <span className="pficon pficon-warning-triangle-o"></span>
+                        {STRAPI_CONFIG_WARNING_MSG}
+                    </strong>
                 </h4>
             </div>
             <div className="mt-2">
